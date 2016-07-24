568f73955519acbec11b1dcc648a49ed57cd4062
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-07-24 18:52:39 +0300

    Crated main 'Character' class

---
+++ Dual-Battle/Character.swift
@@ -0,0 +1,56 @@
+//
+//  Character.swift
+//  Dual-Battle
+//
+//  Created by Mark Laukkanen on 24/07/16.
+//  Copyright © 2016 Mark Laukkanen. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    private var _HP: Int = 0
+    private var _attackPwr: Int = 0
+    private var _name: String = "Player 1"
+    
+    var HP: Int {
+        get {
+            return _HP
+        }
+    }
+    
+    var attackPwr: Int {
+        get {
+            return _attackPwr
+        }
+    }
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var isAlive: Bool {
+        get {
+            if HP <= 0 {
+                return false
+            } else {
+                return true
+            }
+        }
+    }
+    
+    init(startingHp: Int, attackPwr: Int) {
+        self._HP = startingHp
+        self._attackPwr = attackPwr
+    }
+    
+    func attemptAttack(attackPwr: Int) -> Bool {
+        self._HP -= attackPwr
+        return true
+    }
+    
+    
+    
+}
